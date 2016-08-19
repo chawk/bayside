a572dd928bc45f7847c6059b26bbe402232a9b74
chawk <chrshawkes@yahoo.com>
2016-08-18 21:41:51 -0400

    Updated Bayside Admin to Create Sample Project Files

--- baysideAdmin.js
+++ baysideAdmin.js
@@ -1,6 +1,16 @@
 var fs = require('fs'),
-        self = this;
+        self = this
+        dir = './static',
+        dir2 = './static/css'
 
+// create these directories.  
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
+if (!fs.existsSync(dir2)){
+    fs.mkdirSync(dir2);
+}
 
 createBaseProject = function () {
     return `
@@ -16,11 +26,44 @@ createBaseProject = function () {
     `
 }
 
-fs.writeFile("./" + process.argv[2] + ".js", createBaseProject(), function(err) {
+createCss = function () {
+    return `
+        h1 { color: red }
+    `
+}
 
+createIndex = function () {
+    return `
+        <html>
+            <head>
+                <title>Sample Bayside App</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1">
+                <link rel="stylesheet" href="{root}/static/css/main.css" />
+            </head>
+            <body>
+                <h1>Your Bayside App is Running!</h1>
+            </body>
+        </html>
+    `
+}
+
+
+fs.writeFile("./" + process.argv[2] + ".js", createBaseProject(), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+});
+
+fs.writeFile("./static/css/main.css", createCss(), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+});
+
+fs.writeFile("./index.html", createIndex(), function(err) {
     if(err) {
         return console.log(err);
     }
 
-    console.log("A Sample Bayside App Was Created!");
+    console.log("Your Bayside App Was Created!")
 });
